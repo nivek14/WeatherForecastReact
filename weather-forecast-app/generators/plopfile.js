e9a8173e0ead6be9f3e2451f5bd15836c97ca687
kevin oliveira <kevinoliveiracc14@gmail.com>
2025-10-25 02:35:56 -0300

    feat: ajuste gitignore e adição do plop para criação de componentes de forma automatizada

---
+++ weather-forecast-app/generators/plopfile.js
@@ -0,0 +1,30 @@
+module.exports = function (plop) {
+    // component generator
+    plop.setGenerator("Component", {
+        description: "application component logic",
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "Component name",
+            },
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "../src/presentation/components/content/{{ pascalCase name }}/index.tsx",
+                templateFile: "templates/index.tsx.hbs",
+            },
+            {
+                type: "add",
+                path: "src/components/{{ pascalCase name }}/{{ pascalCase name }}.styles.ts",
+                templateFile: "templates/styles.ts.hbs",
+            },
+            {
+                type: "add",
+                path: "src/components/{{ pascalCase name }}/{{ pascalCase name }}.types.ts",
+                templateFile: "templates/types.ts.hbs",
+            },
+        ],
+    });
+};
